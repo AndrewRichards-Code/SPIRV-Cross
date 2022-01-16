1a92c6bc086b339689578d174694f9360db96605
Andrew Richards <andyjrichards93@gmail.com>
2022-01-16 12:14:56 +0000

    Commit build folder.

---
+++ build/gitversion.h
@@ -0,0 +1,9 @@
+// Copyright 2016-2021 The Khronos Group Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef SPIRV_CROSS_GIT_VERSION_H_
+#define SPIRV_CROSS_GIT_VERSION_H_
+
+#define SPIRV_CROSS_GIT_REVISION "Git commit: 37dfb3f4 Timestamp: 2022-01-03T11:19:03"
+
+#endif
